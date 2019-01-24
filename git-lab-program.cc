2a4dae9b67dc5a534a9605a99809b9c121fcb981
Drew <de162518@ohio.edu>
2019-01-23 19:41:05 -0500

    @markamay

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Drew Edmonds
+ *        Date: 1-23-19
+ * Description: 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}
